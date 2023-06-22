b5e40a9eda5960188d5df1e94223da9ae4cfdfbb
CanseFr <jucansell@gmail.com>
2023-06-22 20:40:27 +0200

    22/06/2023 - Test Cleaning methode name

--- discord/src/test/java/com/canse/discord/test_integration/controller/UserControllerTest.java
+++ discord/src/test/java/com/canse/discord/test_integration/controller/UserControllerTest.java
@@ -1,6 +1,10 @@
 package com.canse.discord.test_integration.controller;
+
+import com.canse.discord.dto.UserDto;
+import com.canse.discord.models.Role;
 import com.canse.discord.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -9,10 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest
 public class UserControllerTest {
@@ -20,10 +26,22 @@ public class UserControllerTest {
     @Autowired
     private WebApplicationContext context;
     private MockMvc mvc;
+    @MockBean
+    private UserServiceImpl userService;
 
     @BeforeEach
     public void init() throws Exception {
         this.mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+
+        UserDto userBeaforEach = UserDto.builder()
+                .email("ssssadam@hussein.sisi")
+                .password("salamalek")
+                .firstname("satan")
+                .lastname("sadame")
+                .roles(new ArrayList<>(List.of(new Role(1,"ROLE_ADMIN"))))
+                .build();
+
+        Mockito.when(userService.findById(1)).thenReturn(userBeaforEach);
     }
 
     // DELETE
@@ -71,6 +89,4 @@ public class UserControllerTest {
     void undefinedUserGetUserlList_Exception() throws Exception {
         mvc.perform(get("/user/")).andExpect(status().isForbidden());
     }
-
-
 }
